02ff84006be82636cdfcc22e7d0e601c003be569
Bruno Mota <bruno.mota@kuehne-nagel.com>
2025-01-20 11:47:11 +0000

    Corrected requests.sh script

--- spring-examples/spring-prometheus-grafana/scripts/requests.sh
+++ spring-examples/spring-prometheus-grafana/scripts/requests.sh
@@ -2,14 +2,14 @@
 
 REQUEST_COUNT=100
 
-methods=("GET" "POST" "PUT" "DELETE")
+methods=("PATCH")
 
 make_request() {
   method=${methods[ $RANDOM % ${#methods[@]} ]}
 
   echo "Making $method request"
 
-  curl -X $method http://localhost:8080/api/v1/hello -s -o /dev/null -w "%{http_code}\n"
+  curl -X $method http://localhost:666/api/v1/hello -s -o /dev/null -w "%{http_code}\n"
 }
 
 for ((i=0; i<$REQUEST_COUNT; i++))
--- spring-examples/spring-test-containers/src/main/java/com/springtestcontainers/JpaItemRepository.java
+++
@@ -1,4 +0,0 @@
-package com.springtestcontainers;
-
-public class JpaItemRepository {
-}
